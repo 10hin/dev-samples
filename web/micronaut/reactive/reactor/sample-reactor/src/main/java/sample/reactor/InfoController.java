b271f9fed0a632b16c8e2ed5d1e0c04a44c37754
10hin <10hin@outlook.jp>
2022-09-02 17:53:53 +0000

    add info controller

---
+++ web/micronaut/reactive/reactor/sample-reactor/src/main/java/sample/reactor/InfoController.java
@@ -0,0 +1,12 @@
+package sample.reactor;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/info")
+public class InfoController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+}
