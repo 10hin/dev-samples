caf3aa29ea38f932b60bc8299f9afc88d6ab7056
10hin <10hin@outlook.jp>
2022-09-03 02:03:09 +0900

    set target java version to 17

--- web/micronaut/sample-maven/src/main/java/sample/maven/InfoController.java
+++ web/micronaut/sample-maven/src/main/java/sample/maven/InfoController.java
@@ -5,8 +5,13 @@ import io.micronaut.http.annotation.*;
 @Controller("/info")
 public class InfoController {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "info";
+    @Get(uri="/", produces="")
+    public Map<String, Object> index() {
+        return Map.of(
+                "programming-langage", "Java",
+                "web-framework", "micronaut",
+                "build-tool", "maven"
+        );
     }
+
 }
