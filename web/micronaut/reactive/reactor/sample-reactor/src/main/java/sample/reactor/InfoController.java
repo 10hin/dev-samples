882fe8700561517a8f511121df200551d6576ee3
10hin <10hin@outlook.jp>
2022-09-03 02:56:20 +0900

    implement InfoController

--- web/micronaut/reactive/reactor/sample-reactor/src/main/java/sample/reactor/InfoController.java
+++ web/micronaut/reactive/reactor/sample-reactor/src/main/java/sample/reactor/InfoController.java
@@ -1,12 +1,20 @@
 package sample.reactor;
 
 import io.micronaut.http.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Controller("/info")
 public class InfoController {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
+    @Get(uri="/", produces="application/json; charset=utf-8")
+    public Mono<Map<String, Object>> index() {
+        return Mono.just(Map.of(
+                "programming-language", "Java",
+                "web-framework", "micronaut",
+                "build-tool", "maven"
+        ));
     }
+
 }
