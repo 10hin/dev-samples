727be456395e639f1e7353f5f3b00a601014e337
10hin <10hin@outlook.jp>
2022-09-03 03:23:04 +0900

    implement InfoController

--- web/micronaut/reactive/rxjava3/sample-rxjava3/src/main/java/sample/rxjava3/InfoController.java
+++ web/micronaut/reactive/rxjava3/sample-rxjava3/src/main/java/sample/rxjava3/InfoController.java
@@ -1,12 +1,20 @@
 package sample.rxjava3;
 
 import io.micronaut.http.annotation.*;
+import io.reactivex.rxjava3.core.Single;
+
+import java.util.Map;
 
 @Controller("/info")
 public class InfoController {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
+    @Get(uri="/", produces="application/json; charset=utf-8")
+    public Single<Map<String, Object>> index() {
+        return Single.just(Map.of(
+                "programming-language", "Java",
+                "web-framework", "micronaut",
+                "build-tool", "maven"
+        ));
     }
+
 }
